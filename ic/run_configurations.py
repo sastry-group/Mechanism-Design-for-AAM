fb81b63048cd9fc25f483d61d8c53b6522b4bd09
Gaby <mgmendoza@alumni.usc.edu>
2025-02-17 15:48:28 -0800

    fixing some changes not captured in the main

--- ic/run_configurations.py
+++ ic/run_configurations.py
@@ -9,7 +9,9 @@ default_good_valuation_values = [1]
 price_default_good_values = [1]
 lambda_frequency_values = [30]
 price_upper_bound_values = [3000]
-num_agents_to_run = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,177]
+num_agents_to_run = [10]
+# num_agents_to_run = [10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,177]
+tol_error_to_check = [0.1, 0.01, 0.001]
 # num_agents_to_run = [170]
 # num_CPUS = 1
 
@@ -45,8 +47,10 @@ for file in file_list:
             "--lambda_frequency", str(lambda_frequency),
             "--price_upper_bound", str(price_upper_bound),
             "--num_agents_to_run", str(num_agents_to_run),
-            "--run_up_to_auction", str(70)
-        ]
+            "--run_up_to_auction", str(70),
+            "--tol_error_to_check"
+        ] + [str(tol) for tol in tol_error_to_check]
+        
 
         print(f"Running configuration {idx + 1}/{len(parameter_combinations)}: "
             f"BETA={BETA}, dropout_good_valuation={dropout_good_valuation}, "
