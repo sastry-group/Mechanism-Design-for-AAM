e725e8fb482ae37d57219f936c1992f1e9ee646c
Victoria Tuck <victoria_tuck@berkeley.edu>
2025-02-26 10:39:54 -0800

    Adding file to create cases with different taus

---
+++ test_cases/change_tau.py
@@ -0,0 +1,40 @@
+import json
+
+filename = "test_cases/toulouse_case_cap4_updated_20.json"
+
+# Load your original JSON file
+with open(filename, 'r') as infile:
+    data = json.load(infile)
+
+factor = 0.25
+
+# 15 is the number of seconds that 1 time step in the original case represents
+seconds = int(round(15 / factor))
+
+# Process the flights section to update time values
+for flight in data.get('flights', {}).values():
+    # Multiply appearance_time by factor
+    if 'appearance_time' in flight:
+        new_time = flight['appearance_time'] * factor
+        flight['appearance_time'] = round(new_time)
+
+    # Process each request inside the flight
+    for request in flight.get('requests', {}).values():
+        if 'request_departure_time' in request:
+            new_time = request['request_departure_time'] * factor
+            request['request_departure_time'] = round(new_time)
+        if 'request_arrival_time' in request:
+            new_time = request['request_arrival_time'] * factor
+            request['request_arrival_time'] = round(new_time)
+        if 'sector_times' in request:
+            new_times = [round(sector_time * factor) for sector_time in request['sector_times']]
+            request['sector_times'] = new_times
+
+new_time = data.get('timing_info', {})['end_time'] * factor
+data.get('timing_info', {})['end_time'] = round(new_time)
+new_time = data.get('timing_info', {})['auction_frequency'] * factor
+data.get('timing_info', {})['auction_frequency'] = round(new_time)
+
+# Save the updated JSON to a new file
+with open(f"{filename[:-7]}{int(20*factor)}stepauction_{seconds}sectau.json", 'w') as outfile:
+    json.dump(data, outfile, indent=4)
